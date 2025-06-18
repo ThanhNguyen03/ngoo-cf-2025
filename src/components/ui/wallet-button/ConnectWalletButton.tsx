9aad64b52fa1be9798d7c051cd9b6fbd95fe4914
ThanhNguyen03 <thanhnguyen25011207@gmail.com>
2025-06-18 18:20:55 +0700

    add UI for wallet button

---
+++ src/components/ui/wallet-button/ConnectWalletButton.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import type { Address } from 'viem'
+import { useBalance } from 'wagmi'
+
+import { cn, formatBalance } from '@/utils'
+import { WalletIcon } from '@phosphor-icons/react'
+import Image from 'next/image'
+import { AccountModal, ConnectWalletModal } from '../modal'
+import { defaultAvatar } from '@/images'
+
+type TConnectWalletButtonProps = {
+  accountButtonclassName?: string
+  connectWalletButtonClassName?: string
+  address: Address | undefined
+  isConnected: boolean
+}
+
+export const ConnectWalletButton = ({
+  accountButtonclassName,
+  connectWalletButtonClassName,
+  address,
+  isConnected,
+}: TConnectWalletButtonProps) => {
+  const { data: balance } = useBalance({ address })
+  const [isConnectWalletModalOpen, setIsConnectWalletModalOpen] =
+    useState<boolean>(false)
+  const [isAccountModalOpen, setIsAccountModalOpen] = useState<boolean>(false)
+
+  return (
+    <>
+      {isConnected ? (
+        <button
+          className={cn(
+            'rounded-2 flex h-10 cursor-pointer items-center justify-center gap-2 border border-white/10 bg-white/10 px-4 py-2 font-semibold text-white shadow',
+            accountButtonclassName
+          )}
+          onClick={() => setIsAccountModalOpen(true)}
+        >
+          <p>
+            {balance &&
+              formatBalance(
+                balance.value,
+                balance.decimals,
+                balance.symbol
+              ).toPrecision()}
+          </p>
+          <Image alt='default-avt' src={defaultAvatar} className='size-4' />
+        </button>
+      ) : (
+        <button
+          className={cn(
+            'rounded-2 flex h-10 cursor-pointer items-center justify-center gap-2 bg-blue-500 px-4 py-2 font-semibold text-white shadow',
+            connectWalletButtonClassName
+          )}
+          onClick={() => setIsConnectWalletModalOpen(true)}
+        >
+          <WalletIcon />
+          Connect Wallet
+        </button>
+      )}
+      <ConnectWalletModal
+        isOpen={isConnectWalletModalOpen}
+        onClose={() => setIsConnectWalletModalOpen(false)}
+      />
+      <AccountModal
+        isOpen={isAccountModalOpen}
+        onClose={() => setIsAccountModalOpen(false)}
+      />
+    </>
+  )
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,35 @@
 import { twMerge } from 'tailwind-merge'
 import { ClassValue, clsx } from './clsx'
+import { formatUnits } from 'viem'
 
 export function cn(...args: ClassValue[]) {
   return twMerge(clsx(args))
+}
+
+export const truncateAddress = (
+  address?: string,
+  start: number = 4,
+  end: number = 4
+): string => {
+  if (typeof address !== 'string' || address.length <= start + end) {
+    return address ?? ''
+  }
+
+  return `${address.slice(0, start)}...${address.slice(-end)}`
+}
+
+export const formatBalance = (
+  value: bigint,
+  decimals: number,
+  symbol: string
+) => {
+  const formattedValue = formatUnits(value, decimals)
+
+  return {
+    toFixed: (fractionDigits: number = 5) =>
+      `${Number(formattedValue).toFixed(fractionDigits)} ${symbol}`,
+    toPrecision: (precision: number = 5) =>
+      `${parseFloat(formattedValue).toPrecision(precision)} ${symbol}`,
+    rawValue: `${formattedValue} ${symbol}`,
+  }
 }
