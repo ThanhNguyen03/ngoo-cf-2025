1e54d18d7da86932fe8d627b7b1364581662529b
ThanhNguyen03 <thanhnguyen25011207@gmail.com>
2025-06-19 19:29:18 +0700

    add imgs for hero and update favicon

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,1 +1,4 @@
 export { default as defaultAvatar } from './default-avt.png'
+export { default as heroBanner } from './hero-banner.svg'
+export { default as animatedCoffeeShop } from './animated-coffee-shop.gif'
+export { default as ngooCfText } from './ngoo-cf-text.svg'
