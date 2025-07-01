e02169c00201ea72a541fb206e437d25606e4184
thanhf.ng_ <thanhfng.dev@gmail.com>
2025-07-02 00:09:42 +0700

    add button purchase in item card

--- src/components/section/best-seller.tsx
+++ src/components/section/best-seller.tsx
@@ -1,28 +1,33 @@
 'use client'
+
 import { iceCube, leaf } from '@/images'
 import { liptonTea } from '@/products'
-import { TItemCardProps } from '@/types'
+import { TItem } from '@/types'
 import Image from 'next/image'
 import ItemCard from '../ui/ItemCard'
 import { Slider } from '../ui/Slider'
+import ItemDetailModal from '../ui/modal/ItemDetailModal'
+import { useState } from 'react'
 
-const MOCKED_ITEMS_DATA: TItemCardProps[] = [
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$', salePrice: '10$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$', salePrice: '10$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$', salePrice: '8$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$', salePrice: '5$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$' },
-  { image: liptonTea, title: 'Lipton Xi Muoi', price: '12$', salePrice: '9$' },
+const MOCKED_ITEMS_DATA: TItem[] = [
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12, amountDiscount: 10 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12, amountDiscount: 10 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12, amountDiscount: 8 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12, amountDiscount: 5 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12 },
+  { image: liptonTea, title: 'Lipton Xi Muoi', price: 12, amountDiscount: 9 },
 ]
 
 const BestSeller = () => {
+  const [selectedItem, setSelectedItem] = useState<TItem | undefined>()
+
   return (
     <section className='relative overflow-hidden bg-beige-100/10'>
-      <div className='absolute bottom-0 -left-2'>
+      <div className='absolute z-20 bottom-0 -left-2'>
         <Image
           alt='ice-cube'
           src={iceCube}
@@ -31,7 +36,7 @@ const BestSeller = () => {
           className='size-50 object-cover'
         />
       </div>
-      <div className='absolute top-0 -right-2'>
+      <div className='absolute z-20 top-0 -right-2'>
         <Image
           alt='leaf'
           src={leaf}
@@ -40,25 +45,30 @@ const BestSeller = () => {
           className='size-70 object-cover'
         />
       </div>
-      <div className='px-2 pt-6 pb-10 md:px-6 md:pt-10 md:pb-20 lg:px-10 lg:pt-20 lg:pb-30'>
+      <div className='px-2 pt-6 pb-10 md:px-6 md:pt-10 md:pb-20 lg:px-10 lg:pt-20 lg:pb-30 z-0'>
         <div className='relative mx-auto flex w-full max-w-[1024px] flex-col gap-2 md:gap-6'>
           <h2 className='text-44 md:text-55 font-lobster mx-auto w-fit bg-[url(/images/title-background.jpg)] bg-clip-text text-center font-black text-transparent'>
             Best Seller
           </h2>
 
-          <Slider numsItemsPerSlice={3} isDot>
+          <Slider numsItemsPerSlice={3} isDot onPause={!!selectedItem}>
             {MOCKED_ITEMS_DATA.map((item, i) => (
               <ItemCard
                 key={`${item.title}-${i}`}
-                image={item.image}
-                title={item.title}
-                price={item.price}
-                salePrice={item.salePrice}
+                data={item}
+                setSelectedItem={setSelectedItem}
               />
             ))}
           </Slider>
         </div>
       </div>
+      {selectedItem && (
+        <ItemDetailModal
+          isOpen={!!selectedItem}
+          onClose={() => setSelectedItem(undefined)}
+          data={selectedItem}
+        />
+      )}
     </section>
   )
 }
--- src/components/ui/ItemCard.tsx
+++ src/components/ui/ItemCard.tsx
@@ -1,41 +1,65 @@
-import { TItemCardProps } from '@/types'
+import { TItem } from '@/types'
 import { cn } from '@/utils'
 import Image from 'next/image'
-import { FC } from 'react'
+import { Dispatch, FC, SetStateAction } from 'react'
 import SaleTagIcon from './icons/SaleTag'
+import Button from './Button'
+import { PlusIcon } from '@phosphor-icons/react/dist/ssr'
 
-const ItemCard: FC<TItemCardProps> = ({ image, title, price, salePrice }) => {
+type TItemCardProps = {
+  data: TItem
+  setSelectedItem: Dispatch<SetStateAction<TItem | undefined>>
+}
+
+const ItemCard: FC<TItemCardProps> = ({ data, setSelectedItem }) => {
   return (
-    <div className='border-dark-600/10 rounded-2 bg-beige-50 relative max-w-60 border'>
-      {/* sale tag */}
-      <div className='absolute top-5 left-0'>
-        <SaleTagIcon size={20} />
-      </div>
-      <Image
-        alt={`image-${title}`}
-        src={image}
-        width={240}
-        height={240}
-        className='rounded-t-2 size-60 object-cover'
-      />
+    <>
+      <div className='border-dark-600/10 rounded-2 bg-beige-50 relative max-w-60 border'>
+        {/* sale tag */}
+        <div className='absolute top-5 left-0'>
+          <SaleTagIcon
+            width={40}
+            height={20}
+            amountDiscount={data.amountDiscount || 0}
+          />
+        </div>
+        <Image
+          alt={`image-${data.title}`}
+          src={data.image}
+          width={240}
+          height={240}
+          className='rounded-t-2 size-60 object-cover'
+        />
 
-      <div className='flex flex-col items-start gap-2 p-2 md:gap-3 md:p-4'>
-        <h4 className='text-16! text-dark-600 font-semibold'>{title}</h4>
-        <div className='flex items-end justify-center gap-2'>
-          {salePrice && (
-            <p className='text-14! text-dark-600/70'>{salePrice}</p>
-          )}
-          <p
-            className={cn(
-              'text-14! text-dark-600/70',
-              !!salePrice && 'text-dark-600/50 text-12! line-through',
-            )}
-          >
-            {price}
-          </p>
+        <div className='flex flex-col items-start gap-2 p-2 md:gap-3 md:p-4'>
+          <h4 className='text-16! text-dark-600 font-semibold'>{data.title}</h4>
+          <div className='flex justify-between w-full items-center'>
+            <div className='flex items-end justify-center gap-2'>
+              {data.amountDiscount && (
+                <p className='text-14! text-dark-600/70'>
+                  {data.price - (data.price * data.amountDiscount) / 100}$
+                </p>
+              )}
+              <p
+                className={cn(
+                  'text-14! text-dark-600/70',
+                  !!data.amountDiscount &&
+                    'text-dark-600/50 text-12! line-through',
+                )}
+              >
+                {data.price}$
+              </p>
+            </div>
+            <Button
+              className='bg-green-500 rounded-full w-10 h-fit py-1 px-0 gap-0 text-16!'
+              icon={<PlusIcon size={12} className='text-beige-50' />}
+              disableAnimation
+              onClick={() => setSelectedItem(data)}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
